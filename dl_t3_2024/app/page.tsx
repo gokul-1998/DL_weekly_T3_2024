6d8c8d6c46a4fbc13ac8e3fe23d472467ba9caca
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-10-20 09:43:19 +0530

    updated more

--- dl_t3_2024/app/page.tsx
+++ dl_t3_2024/app/page.tsx
@@ -1,10 +1,13 @@
+// app/page.tsx
+import { getRoutes } from '../utils/getRoutes'; // Adjust the path if necessary
 
+export default async function Home() {
+  const routes = await getRoutes(); // Fetch the folders in the component
 
-export default function Home({ routes }) {
   return (
     <div>
       <h1>My Homepage</h1>
-      <h2>Available Routes:</h2>
+      <h2>Available Folders:</h2>
       <ul>
         {routes.map(({ route }) => (
           <li key={route}>
